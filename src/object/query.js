5cad6285c92ba1485f62b6e7a1b6a4b4ebe3f035
yunwisdom <qq695926920@163.com>
2020-12-25 15:49:07 +0800

    init

--- src/object/query.js
+++ src/object/query.js
@@ -435,13 +435,12 @@ const query = {
                 }
 
                 //设置system_userinfo
-                storage.setStore('system_linfo', JSON.stringify({ username: response.body.userinfo.userid, password: '************' }), 3600 * 24 * 30);
-                storage.setStore('system_userinfo', JSON.stringify(response.body.userinfo), 3600 * 24 * 30);
+                storage.setStore('system_linfo', JSON.stringify({ username: response && response.body && response.body.userinfo ? response.body.userinfo.userid || response.body.userinfo.username : '', password: '************' }), 3600 * 24 * 30);
+                storage.setStore('system_userinfo', JSON.stringify(response && response.body && response.body.userinfo ? response.body.userinfo : {}), 3600 * 24 * 30);
                 storage.setStore('system_token', JSON.stringify(code), 3600 * 24 * 30);
-                storage.setStore('system_department', JSON.stringify(response.body.userinfo.department), 3600 * 24 * 30);
+                storage.setStore('system_department', JSON.stringify(response && response.body && response.body.userinfo ? response.body.userinfo.department || {} : {}), 3600 * 24 * 30);
                 storage.setStore('system_login_time', dayjs().format('YYYY-MM-DD HH:mm:ss'), 3600 * 24 * 30);
                 storage.setStore(`sys_wework_user_code#wework_user_code#@${code}`, JSON.stringify(userinfo), 3600 * 24 * 30);
-
             } else {
                 userinfo = storage.getStore('system_userinfo');
             }
