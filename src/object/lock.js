669ea1af6db1269b5b99105d824f9165765b69fc
yunwisdom <qq695926920@163.com>
2021-01-05 19:03:53 +0800

    init

--- src/object/lock.js
+++ src/object/lock.js
@@ -5,7 +5,7 @@ const lock = {
      * @param {*} lockName
      * @param {*} lockMS
      */
-    async lock(lockName = 'crontab_task', lockMS = 24 * 3600 * 1000) {
+    async lock(lockName = 'crontab_task', lockMS = 24 * 3600 * 1000, lockOperator = '') {
         try {
             const ctime = dayjs().format('YYYY-MM-DD hh:mm:ss');
             const lockList = await Betools.query.queryTableDataByWhereSQL('bs_lock_info', `_where=(lock_name,eq,${lockName})~and(status,eq,1)~and(expire_time,gt,${ctime})&_sort=-id`);
@@ -25,7 +25,7 @@ const lock = {
                     content: '',
                     status: 1,
                     teamname: '',
-                    lock_username: '',
+                    lock_username: lockOperator,
                     lock_flag: 'Y',
                 };
                 await Betools.manage.postTableData('bs_lock_info', elem);
